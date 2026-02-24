fca22f882204ea3f2f1815d59179a46311c386cb
Sovann <42709993+voeurngsovann@users.noreply.github.com>
2026-02-24 14:25:11 +0700

    Update install_os_packages.sh
    package to perform all prerequisites.

--- scripts/install_os_packages.sh
+++ scripts/install_os_packages.sh
@@ -40,5 +40,32 @@ dnf install -y ksh
 dnf install -y numactl
 dnf install -y numactl-devel
 dnf install compat-openssl10-1.0.2o-4.el8_10.1.x86_64 -y
+cat >> /etc/sysctl.conf <<EOF
+fs.file-max = 6815744  
+kernel.sem = 250 32000 100 128
+kernel.shmmni = 4096
+kernel.shmall = 1073741824
+kernel.shmmax = 4398046511104
+kernel.panic_on_oops = 1
+net.core.rmem_default = 262144
+net.core.rmem_max = 4194304
+net.core.wmem_default = 262144
+net.core.wmem_max = 1048576
+net.ipv4.conf.all.rp_filter = 2
+net.ipv4.conf.default.rp_filter = 2
+fs.aio-max-nr = 1048576
+net.ipv4.ip_local_port_range = 9000 65500
+EOF
+/sbin/sysctl -p
 
+cat >> /etc/security/limits.d/oracle-database-preinstall-19c.conf <<EOF
+oracle   soft   nofile    1024
+oracle   hard   nofile    65536
+oracle   soft   nproc    16384
+oracle   hard   nproc    16384
+oracle   soft   stack    10240
+oracle   hard   stack    32768
+oracle   hard   memlock    134217728
+oracle   soft   memlock    134217728
+EOF
 
